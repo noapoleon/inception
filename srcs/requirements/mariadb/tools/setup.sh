a616c6409487b29f71cd38de394d97dcd50dbf63
Noapoleon <noalegrand088@gmail.com>
2024-05-26 02:47:20 +0200

    stuff

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -4,22 +4,22 @@
 service mariadb start
 
 # Create db
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
 
 # Create db user
-mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASS}';"
 
 # Give all privileges to new user
-mariadb -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASS}';"
 
 # Give a password to root user
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';"
 
 # Update changes with flush
 mariadb -e "FLUSH PRIVILEGES;"
 
 # restart mysql
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+mysqladmin -u root -p$DB_ROOT_PASS shutdown
 
 # Replace PID 1
 exec mysqld
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+sleep 10s
+
+# config wordpress with cli
+wp-cli.phar config create	--allow-root \
+					--dbname=${DB_NAME} \
+					--dbuser=${DB_USER} \
+					--dbpass=${DB_PASS} \
+					--dbhost=mariadb:3306 \
+					--path=/var/www/wordpress
+
 # create php run dir to prevent error
 mkdir -p /run/php
 
